c4a4aa2a05031e10e7979054f48bc17d834e850a
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2020-10-30 09:28:17 -0600

    finishes Header.js and Tabs.js

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // Creating Elements
+    const header = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+    // Creating Dom Hierarchy
+    header.appendChild(headerDate);
+    header.appendChild(h1);
+    header.appendChild(headerTemp);
+    // Adding Classes
+    header.classList.add('header');
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp');
+    // Adding Content
+    headerDate.textContent = 'March 28, 2020';
+    h1.textContent = 'Lambda Times';
+    headerTemp.textContent = '98°';
+    // console.log(header);
+    // Return Statement
+    return header
+}
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,22 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+const topicsContainer = document.querySelector('.topics');
+axios
+    .get('https://lambda-times-api.herokuapp.com/topics')
+    .then((res) => {
+        console.log(res);
+        const topics = res.data.topics;
+        topics.forEach((tab) => {
+            const tabName = tab;
+            console.log(tabName);
+            const newTab = document.createElement('div');
+            newTab.classList.add('tab');
+            topicsContainer.appendChild(newTab);
+            console.log(topicsContainer);
+            newTab.textContent = tabName;
+        })
+    })
+    .catch((err) => {
+        console.log('Something went wrong', err);
+    })
